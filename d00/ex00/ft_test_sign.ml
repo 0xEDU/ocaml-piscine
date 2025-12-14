0761ea4b9425701160179771172a67ed62855e76
Eduardo Tachotte <etachott@student.42sp.org.br>
2025-12-14 17:01:56 -0300

    Add some tests

--- d00/ex00/ft_test_sign.ml
+++ d00/ex00/ft_test_sign.ml
@@ -4,3 +4,8 @@ let ft_test_sign(n : int) : unit =
     else
         print_endline "negative"
 
+
+(* tests *)
+(* let _ = ft_test_sign(42) *)
+(* let _ = ft_test_sign(0) *)
+(* let _ = ft_test_sign(-42) *)
