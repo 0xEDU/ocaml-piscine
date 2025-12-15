c2fb250e3db0658fb2282794f79a6825c42d22ad
Eduardo Tachotte <bfwaend@gmail.com>
2025-12-14 23:44:03 -0300

    d00/ex02

---
+++ d00/ex02/ft_power.ml
@@ -0,0 +1,16 @@
+let rec ft_power base exponent = 
+    if exponent = 0 then
+        1
+    else if exponent = 1 then
+        base
+    else
+        ft_power (base * base) (exponent / 2)
+
+
+(* tests *)
+let _ = ft_power 2 4 |> print_int; print_newline ()
+let _ = ft_power 3 0 |> print_int; print_newline ()
+let _ = ft_power 0 5 |> print_int; print_newline ()
+let _ = ft_power 2 63 |> print_int; print_newline ()
+
+
