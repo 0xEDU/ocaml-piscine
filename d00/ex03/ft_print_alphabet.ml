898436c9d4afc264f1363baab5d3003132e47326
Eduardo Tachotte <bfwaend@gmail.com>
2025-12-16 00:20:57 -0300

    d00/ex03

---
+++ d00/ex03/ft_print_alphabet.ml
@@ -0,0 +1,9 @@
+let ft_print_alphabet () : unit = 
+    let z = int_of_char 'z' in
+    let rec loop c =
+        if c > z then print_newline()
+        else (print_char(char_of_int c); loop (c + 1))
+    in loop(int_of_char 'a')
+
+(* tests *)
+let _ = ft_print_alphabet ()
