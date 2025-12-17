5babcd857ad06b15ccaf95e988d6781ae483a78f
Eduardo Tachotte <bfwaend@gmail.com>
2025-12-17 09:54:59 -0300

    d00/ex04

--- d00/ex04/ft_print_comb.ml
+++ d00/ex04/ft_print_comb.ml
@@ -1,24 +1,25 @@
-let ft_print_comb () = 
+let ft_print_comb () : unit =
     let print_number x y z =
         print_int x;
         print_int y;
         print_int z;
-        if x != 7 || y != 8 || z != 9 then print_string ", "
+        if x != 7 then print_string ", "
     in
 
-    let rec loop2 x y z =
-        if z != 10 then (print_number x y z; loop2 x y (z + 1))
+    let rec for_each_z x y z =
+        if z < 10 then (print_number x y z; for_each_z x y (z + 1))
     in
 
-    let rec loop1 x y =
-        if y != 9 then (loop2 x y 2; loop1 x (y + 1))
+    let rec for_each_y x y =
+        if y < 9 then (for_each_z x y (y + 1); for_each_y x (y + 1))
     in
 
-    let rec loop0 x =
-        if x != 8 then (loop1 x 1; loop0 (x + 1))
-    in loop0 0;
+    let rec for_each_x x =
+        if x < 8 then (for_each_y x (x + 1); for_each_x (x + 1))
+    in for_each_x 0;
 
     print_string "\n"
+ 
 
 (* tests *)
 let _ = ft_print_comb()
