f77d7ac10926d79514953f14400c82d638098996
edu <bfwaend@gmail.com>
2025-12-19 21:02:21 -0300

    d00/ex09

--- d00/ex09/ft_print_comb2.ml
+++ d00/ex09/ft_print_comb2.ml
@@ -1,37 +1,26 @@
 let ft_print_comb2 () : unit =
-    let print_number a b c d =
+    let print_number a b =
+        if a < 10 then print_int 0;
         print_int a;
-        print_int b;
+
         print_string " ";
-        print_int c;
-        print_int d;
-        if b != 8 then print_string ", "
-    in
 
-    let rec iterate_d a b c d =
-        if d < 10 then (
-            print_number a b c d;
-            iterate_d a b c (d + 1)
-        )
-    in
+        if b < 10 then print_int 0;
+        print_int b;
 
-    let rec iterate_c a b c =
-        if c < 10 then (
-            iterate_d a b c 0;
-            iterate_c a b (c + 1)
-        )
+        if a < 98 then print_string ", "
     in
 
     let rec iterate_b a b =
-        if b < 10 then (
-            iterate_c a b 0;
+        if b < 100 then (
+            print_number a b;
             iterate_b a (b + 1)
         )
     in
 
     let rec iterate_a a =
-        if a < 10 then (
-            iterate_b a 0;
+        if a < 99 then (
+            iterate_b a (a + 1);
             iterate_a (a + 1)
         )
     in iterate_a 0;
