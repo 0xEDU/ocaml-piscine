15e28e6d4bccf0b05512e9b53910f46483161414
edu <bfwaend@gmail.com>
2025-12-29 16:48:06 -0300

    d01/ex03

---
+++ d01/ex03/tak.ml
@@ -0,0 +1,15 @@
+let rec tak (x : int) (y : int) (z : int) =
+    if y < x then
+        tak (tak (x - 1) y z) (tak (y - 1) z x) (tak (z - 1) x y)
+    else
+        z
+
+let main () =
+    print_int (tak 1 2 3); print_newline ();
+    print_int (tak 5 23 7); print_newline ();
+    print_int (tak 9 1 0); print_newline ();
+    print_int (tak 1 1 1); print_newline ();
+    print_int (tak 0 42 0); print_newline ();
+    print_int (tak 23498 98734 98776); print_newline ()
+
+let _ = main ()
