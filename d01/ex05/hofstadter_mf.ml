9903aff1405f5731cbd8e5e26a6f69994035735f
edu <bfwaend@gmail.com>
2025-12-29 18:19:32 -0300

    d01/ex05

---
+++ d01/ex05/hofstadter_mf.ml
@@ -0,0 +1,25 @@
+let rec hfs_f (n : int) : int =
+    if n > 0 then
+        n - hfs_m (hfs_f (n - 1))
+    else if n = 0 then
+        1
+    else
+        -1
+
+and hfs_m (n : int) : int =
+    if n > 0 then
+        n - hfs_f(hfs_m (n - 1))
+    else if n = 0 then
+        0
+    else
+        -1
+
+(* tests *)
+let main () =
+    print_int (hfs_m (-42)); print_newline ();
+    print_int (hfs_m 0); print_newline ();
+    print_int (hfs_f 0); print_newline ();
+    print_int (hfs_m 4); print_newline ();
+    print_int (hfs_f 4); print_newline ()
+
+let _ = main ()
