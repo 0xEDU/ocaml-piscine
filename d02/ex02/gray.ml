102afd6acd90dd5dfa6f3a83bfbf64a1e4d70760
edu <bfwaend@gmail.com>
2025-12-31 18:34:47 -0300

    d02/ex02

--- d02/ex02/gray.ml
+++ d02/ex02/gray.ml
@@ -1,20 +1,35 @@
 let gray (n : int) =
-    let rec print_bit n =
-        if n > 1 then print_bit (n lsr 1);
-        print_int (n land 1)
+    if n < 0 then () else
+
+    let rec get_bits x width acc =
+        if width = 0 then
+            acc
+        else
+            get_bits
+                (x lsr 1)
+                (width - 1)
+                (string_of_int (x land 1) ^ acc)
     in
 
     let max_combinations = 1 lsl n in
-    let rec print_gray_bits n =
-        if n < max_combinations then
-            (print_bit (n lxor (n lsr 1)); (* Gray code formula: i XOR (i / 2) *)
+
+    let rec print_gray i =
+        if i < max_combinations then (
+            let gray = i lxor (i lsr 1) in (* Gray code formula: i ^ (i >> 1) *)
+
+            print_string (get_bits gray n "");
             print_char ' ';
-            print_gray_bits (n + 1))
+            print_gray (i + 1)
+        )
     in
-    print_gray_bits 0;
+    print_gray 0;
     print_newline ()
 
 let main () =
-    gray 3
+    print_string "Gray sequence for 0 bits: "; gray 0;
+    print_string "Gray sequence for 1 bits: "; gray 1;
+    print_string "Gray sequence for 2 bits: "; gray 2;
+    print_string "Gray sequence for 3 bits: "; gray 3;
+    print_string "Gray sequence for 4 bits: "; gray 4
 
 let _ = main ()
