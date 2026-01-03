25288512226e3dc9b22441bd3778a46f73178b9d
edu <bfwaend@gmail.com>
2026-01-02 23:45:26 -0300

    Almost there, just need to remove String module references

--- d02/ex03/sequence.ml
+++ d02/ex03/sequence.ml
@@ -1,20 +1,33 @@
 let sequence (n : int) : string =
     if n < 0 then "" else
 
-    let base_sequence = [1] in
+    let base = "1" in
 
-    let rec loop n acc =
-        if n = 0 then
-            acc
+    let count (output, current_char, size) c =
+        if current_char <> c then
+            (output ^ string_of_int size ^ String.make 1 current_char, c, 1)
         else
-            loop (n - 1) base_sequence
+            (output, current_char, size + 1)
     in
-    loop n base_sequence
 
+    let rec iterate sequence n =
+        if n > 0 then
+            let (output, c, size) = String.fold_left count ("", sequence.[0], 0) sequence in
+            iterate (output ^ string_of_int size ^ String.make 1 c) (n - 1)
+        else
+            sequence
+    in
+    iterate base n
 
 (* tests *)
 let main () =
-    print_endline (sequence (-4)); (* should print nothing *)
-    print_endline (sequence 4)
+    print_string "Sequence for n = -1: "; print_endline (sequence (-1));
+    print_string "Sequence for n = 0: "; print_endline (sequence 0);
+    print_string "Sequence for n = 1: "; print_endline (sequence 1);
+    print_string "Sequence for n = 2: "; print_endline (sequence 2);
+    print_string "Sequence for n = 3: "; print_endline (sequence 3);
+    print_string "Sequence for n = 4: "; print_endline (sequence 4);
+    print_string "Sequence for n = 5: "; print_endline (sequence 5);
+    print_string "Sequence for n = 6: "; print_endline (sequence 6)
 
 let _ = main ()
