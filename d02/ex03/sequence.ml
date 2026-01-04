03966f88d3a01991b806b89b5fe76876881ff59c
edu <bfwaend@gmail.com>
2026-01-03 22:06:24 -0300

    d02/ex03

--- d02/ex03/sequence.ml
+++ d02/ex03/sequence.ml
@@ -1,23 +1,32 @@
 let sequence (n : int) : string =
     if n < 0 then "" else
 
-    let base = "1" in
+    let base_case = [1] in
 
-    let count (output, current_char, size) c =
-        if current_char <> c then
-            (output ^ string_of_int size ^ String.make 1 current_char, c, 1)
+    let rec fold_left f acc sequence =
+        match sequence with
+        | [] -> acc
+        | h :: t -> fold_left f (f acc h) t
+    in
+
+    let count (output, current_num, size) c =
+        if current_num <> c then
+            (output @ [size; current_num], c, 1)
         else
-            (output, current_char, size + 1)
+            (output, current_num, size + 1)
     in
 
     let rec iterate sequence n =
         if n > 0 then
-            let (output, c, size) = String.fold_left count ("", sequence.[0], 0) sequence in
-            iterate (output ^ string_of_int size ^ String.make 1 c) (n - 1)
+            match sequence with
+            | h :: _ -> let (output, num, size) = fold_left count ([], h, 0) sequence in
+                        iterate (output @ [size; num]) (n - 1)
+            | [] -> sequence
         else
             sequence
     in
-    iterate base n
+
+    fold_left (fun acc n -> acc ^ string_of_int n) "" (iterate base_case n)
 
 (* tests *)
 let main () =
@@ -25,9 +34,7 @@ let main () =
     print_string "Sequence for n = 0: "; print_endline (sequence 0);
     print_string "Sequence for n = 1: "; print_endline (sequence 1);
     print_string "Sequence for n = 2: "; print_endline (sequence 2);
-    print_string "Sequence for n = 3: "; print_endline (sequence 3);
-    print_string "Sequence for n = 4: "; print_endline (sequence 4);
-    print_string "Sequence for n = 5: "; print_endline (sequence 5);
-    print_string "Sequence for n = 6: "; print_endline (sequence 6)
+    print_string "Sequence for n = 6: "; print_endline (sequence 6);
+    print_string "Sequence for n = 10: "; print_endline (sequence 10)
 
 let _ = main ()
